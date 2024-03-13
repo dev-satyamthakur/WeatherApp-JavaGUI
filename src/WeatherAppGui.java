6ccb78c800db3e3a29b77c6a87bb9dafb3238201
Satyam Thakur <sat.thakur20@gmail.com>
2024-03-13 19:18:50 +0530

    First commit

---
+++ src/WeatherAppGui.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class WeatherAppGui extends JFrame {
+    public WeatherAppGui() {
+        super("Weather App");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(450, 650);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+
+        addGuiComponents();
+    }
+    private void addGuiComponents() {
+        JTextField searchTextField = new JTextField();
+        searchTextField.setBounds(15, 15, 351, 45);
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
+    }
+}
